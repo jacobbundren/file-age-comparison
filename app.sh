5b4e6d415c0e7132086f8d160a91e30864175ab9
Jacob Bundren <jacob.bundren@tpginc.com>
2021-08-18 12:49:20 -0500

    asdf;

--- app.sh
+++ app.sh
@@ -3,18 +3,28 @@
 #  echo > files/${i}.pgp
 #done
 
+# NOTE:
+# File timestamps below are in seconds from epoch
+
 # 30 days (in seconds)
 flagAge=$((30*60*60*24))
+# current time (in seconds) from epoch
+nowTimestamp=$(date +%s)
 
 for file in files/*.pgp
 do
   echo $file
-  nowTimestamp=$(date +%s)
-  fileTimestamp=$(stat -f "%B" $file)
+  # unix
+  #fileTimestamp=$(stat -f "%B" $file)
+  # linux (most likely case. time of last data modification)
+  fileTimestamp=$(stat -c '%y' $file)
+  # age of file
   fileAge=$((nowTimestamp-fileTimestamp))
 
-  if ((flagAge < fileAge))
+  if ((flagAge > fileAge))
   then
     echo $file is old!
+    rm $file
   fi
+
 done
